1a1db289763bd601dc45b69ab3396a543efcfcf5
Boombeam313 <117634209+Boombeam313@users.noreply.github.com>
2023-11-14 10:36:24 +0700

    Revert "ลอง"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-import Question from './component/Question';
-
-function App() {
-  return (
-    <div className="App" >
-      <Question/>
-=======
 import { Route,  Router } from "react-router-dom";
 import Login from "./Login";
 
@@ -18,7 +9,6 @@ function App() {
         <Route path="Login" element={<Login />} />
         <Route path="profile" element={<profile />} />
       </Router>
->>>>>>> 0c83db154517f8acafcc287c37e75e1a3ca67cb6
     </div>
   );
 }
